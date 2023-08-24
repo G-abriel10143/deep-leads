62c9a2025ed33fdca8be070c58f21c36c0786851
JhonnyO3 <jhonatas2004@gmail.com>
2023-08-23 23:09:59 -0300

    Estrutura do projeto Deep Leads

---
+++ src/main/java/core/solution/deepleads/DeepLeadsApplication.java
@@ -0,0 +1,13 @@
+package core.solution.deepleads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepLeadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepLeadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/solution/deepleads/DeepLeadsApplicationTests.java
@@ -0,0 +1,13 @@
+package core.solution.deepleads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepLeadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
